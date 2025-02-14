8558de80b3fbdc12e8595f3217cf1dfd0a5a7524
Mallory Wittwer <mallory.wittwer@epfl.ch>
2025-02-14 15:25:58 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,85 @@
+![EPFL Center for Imaging logo](https://imaging.epfl.ch/resources/logo-for-gitlab.svg)
+# serverkit-trackastra
+
+Implementation of a web server for [Trackastra](https://github.com/weigertlab/trackastra).
+
+Author: EPFL Center for Imaging
+
+## Installing the algorithm server with `pip`
+
+Install dependencies:
+
+```
+pip install -r requirements.txt
+```
+
+Run the server:
+
+```
+uvicorn main:app --host 0.0.0.0 --port 8000
+```
+
+or run `python main.py`. The server will be running on http://localhost:8000.
+
+## Endpoints
+
+A documentation of the endpoints is automatically generated at http://localhost:8000/docs.
+
+**GET endpoints**
+
+- http://localhost:8000/ : Running algorithm message.
+- http://localhost:8000/version : Version of the `imaging-server-kit` package.
+- http://localhost:8000/trackastra/info : Web page displaying project metadata.
+- http://localhost:8000/trackastra/demo : Plotly Dash web demo app.
+- http://localhost:8000/trackastra/parameters : Json Schema representation of algorithm parameters.
+- http://localhost:8000/trackastra/sample_images : Byte string representation of the sample images.
+
+**POST endpoints**
+
+- http://localhost:8000/trackastra/process : Endpoint to run the algorithm.
+
+## Running the server with `docker-compose`
+
+To build the docker image and run a container for the algorithm server in a single command, use:
+
+```
+docker compose up
+```
+
+The server will be running on http://localhost:8000.
+
+## Running the server with `docker`
+
+Build the docker image:
+
+```
+docker build --build-arg PYTHON_VERSION=3.10 -t serverkit-trackastra .
+```
+
+Run the server in a container:
+
+```
+docker run -it --rm -p 8000:8000 serverkit-trackastra:latest
+```
+
+The server will be running on http://localhost:8000.
+
+## Running unit tests
+
+If you have implemented unit tests in the [tests/](./tests/) folder, you can run them using pytest:
+
+```
+pytest
+```
+
+if you are developing your server locally, or
+
+```
+docker run --rm serverkit-trackastra:latest pytest
+```
+
+to run the tests in a docker container.
+
+## Sample images provenance -->
+
+- `trpL_150310-11_img.tif`, `trpL_150310-11_img.tif`: Taken from the [Trackastra repository](https://github.com/weigertlab/trackastra/tree/main/trackastra/data/resources).
---
+++ main.py
@@ -0,0 +1,87 @@
+"""
+Algorithm server definition.
+Documentation: https://github.com/Imaging-Server-Kit/cookiecutter-serverkit
+"""
+from typing import List, Literal, Type
+from pathlib import Path
+import numpy as np
+from pydantic import BaseModel, Field, field_validator
+import uvicorn
+import skimage.io
+import imaging_server_kit as serverkit
+
+# import torch
+from trackastra.model import Trackastra
+from trackastra.tracking import graph_to_napari_tracks
+
+class Parameters(BaseModel):
+    """Defines the algorithm parameters"""
+    image: str = Field(
+        title="Image",
+        description="Input image (2D, 3D).",
+        json_schema_extra={"widget_type": "image"},
+    )
+    mask: str = Field(
+        title="Mask",
+        description="Input segmentation mask (2D, 3D).",
+        json_schema_extra={"widget_type": "labels"},
+    )
+    mode: Literal['greedy', 'greedy_nodiv'] = Field(
+        default='greedy',
+        title="Mode",
+        description="Tracking mode.",
+        json_schema_extra={"widget_type": "dropdown"},
+    )
+
+    @field_validator("image", mode="after")
+    def decode_image_array(cls, v) -> np.ndarray:
+        image_array = serverkit.decode_contents(v)
+        if image_array.ndim not in [2, 3]:
+            raise ValueError("Array has the wrong dimensionality.")
+        return image_array
+
+    @field_validator("mask", mode="after")
+    def decode_mask_array(cls, v) -> np.ndarray:
+        mask_array = serverkit.decode_contents(v)
+        if mask_array.ndim not in [2, 3]:
+            raise ValueError("Array has the wrong dimensionality.")
+        return mask_array
+
+class Server(serverkit.Server):
+    def __init__(
+        self,
+        algorithm_name: str="trackastra",
+        parameters_model: Type[BaseModel]=Parameters
+    ):
+        super().__init__(algorithm_name, parameters_model)
+
+    def run_algorithm(
+        self,
+        image: np.ndarray,
+        mask: np.ndarray,
+        mode: str,
+        **kwargs
+    ) -> List[tuple]:
+        """Runs the algorithm."""
+
+        device = "cpu"  # For now - To avoid CUDA errors
+
+        model = Trackastra.from_pretrained("general_2d", device=device)
+
+        track_graph = model.track(image, mask, mode=mode)  # or mode="ilp", or "greedy_nodiv"
+
+        napari_tracks, napari_tracks_graph, _ = graph_to_napari_tracks(track_graph)
+        
+        return [(napari_tracks, {"name": "Tracks"}, "tracks")]
+
+    def load_sample_images(self) -> List["np.ndarray"]:
+        """Loads one or multiple sample images."""
+        image_dir = Path(__file__).parent / "sample_images"
+        images = [skimage.io.imread(image_path) for image_path in image_dir.glob("*")]
+        return images
+
+server = Server()
+app = server.app
+
+if __name__=='__main__':
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
